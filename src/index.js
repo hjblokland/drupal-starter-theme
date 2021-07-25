0c70f6975dc56fa75f5947a74d2265c4c3d19329
HJ Blokland <hj.blokland@intracto.com>
2021-07-25 12:50:48 +0200

    #35 | Migrate from file-loader to asset-modules

--- src/index.js
+++ src/index.js
@@ -4,9 +4,3 @@ import 'javascripts/svg4everybody.es6'
 
 // Import stylesheets
 import 'stylesheets/main.scss'
-
-// Import icons
-function requireAll(r) {
-  r.keys().forEach(r);
-}
-requireAll(require.context('images/icons/', true, /\.svg$/));
